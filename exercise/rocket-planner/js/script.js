cf285a955dc1131bf84d99ca380ffed5009b991f
Valeria <valery0007@gmail.com>
2021-01-30 21:14:57 +0100

    input value

--- exercise/rocket-planner/js/script.js
+++ exercise/rocket-planner/js/script.js
@@ -1,3 +1,46 @@
+let addItemForm = document.querySelector('#addItemForm');
+let itemsList = document.querySelector('.actionItems');
+
+addItemForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let itemText = addItemForm.elements.namedItem('itemText').value;
+    if (itemText) {
+        renderActionItem(itemText);
+        addItemForm.elements.namedItem('itemText').value = '';
+    }
+
+});
+const renderActionItem = (text) => {
+
+    let element = document.createElement('div');
+    element.classList.add('actionItem__item');
+    let mainElement = document.createElement('div');
+    mainElement.classList.add('actionItem__main');
+    let checkEl = document.createElement('div');
+    checkEl.classList.add('actionItem__check');
+    let textEl = document.createElement('div');
+    textEl.classList.add('actionItem__text');
+    let deleteEl = document.createElement('div');
+    deleteEl.classList.add('actionItem__delete');
+
+    checkEl.innerHTML = ` <div class="actionItem__checkBox">
+                                <i class="fas fa-check" aria-hidden="true"></i>
+                            </div>`
+
+    textEl.textContent = text;
+
+    deleteEl.innerHTML = `
+    <i class="fas fa-times"></i>
+    `
+    mainElement.appendChild(checkEl);
+    mainElement.appendChild(textEl);
+    mainElement.appendChild(deleteEl);
+
+    element.appendChild(mainElement);
+    //   console.log(element);
+    itemsList.prepend(element);
+}
+
 var circle = new ProgressBar.Circle('#container', {
     color: '#65463E',
     // This has to be the same size as the maximum width to
